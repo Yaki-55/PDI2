205af04b5bcaf9d47824e5a19fd6b68962fd1cb4
Yaki55 <mely020202@gs.utm.mx>
2024-11-21 01:41:26 -0600

    fix(practica5): now gradiente add original image to result

--- codigos/practica5.py
+++ codigos/practica5.py
@@ -24,28 +24,28 @@ def main_menu():
                 print("-"*32)
         
         elif choice == '2' and 'image' in locals():
-            kernel_size = int(input("Ingresa el tamaño de la vecindad (ej. 3 para 3x3): "))
+            kernel_size = int(input("Ingresa el tamaño del filtro (ej. 3 para 3x3): "))
             transformed_image = bP.filtro_promedio(image=image, kernel_size=kernel_size)
             if(bP.are_images_equal(transformed_image, image)):
                 print("Son la misma imagen")
             bP.display_results(image, transformed_image, "Filtro Promedio")
         
         elif choice == '3' and 'image' in locals():
-            kernel_size = int(input("Ingresa el tamaño de la vecindad (ej. 3 para 3x3): "))
+            kernel_size = int(input("Ingresa el tamaño del filtro (ej. 3 para 3x3): "))
             transformed_image = bP.filtro_mediano(image=image, kernel_size=kernel_size)
             if(bP.are_images_equal(transformed_image, image)):
                 print("Son la misma imagen")
             bP.display_results(image, transformed_image, "Filtro de la mediana")
         
         elif choice == '4' and 'image' in locals():
-            kernel_size = int(input("Ingresa el tamaño de la vecindad (ej. 3 para 3x3): "))
+            kernel_size = int(input("Ingresa el tamaño del filtro (ej. 3 para 3x3): "))
             transformed_image = bP.filtro_maximo(image=image, kernel_size=kernel_size)
             if(bP.are_images_equal(transformed_image, image)):
                 print("Son la misma imagen")
             bP.display_results(image, transformed_image, "Filtro del maximo")
         
         elif choice == '5' and 'image' in locals():
-            kernel_size = int(input("Ingresa el tamaño de la vecindad (ej. 3 para 3x3): "))
+            kernel_size = int(input("Ingresa el tamaño del filtro (ej. 3 para 3x3): "))
             transformed_image = bP.filtro_minimo(image=image, kernel_size=kernel_size)
             if(bP.are_images_equal(transformed_image, image)):
                 print("Son la misma imagen")
@@ -60,10 +60,12 @@ def main_menu():
             bP.display_results(image, transformed_image2, "Filtro laplaciano sumado a la imagen original")
         
         elif choice == '7' and 'image' in locals():
-            transformed_image = bP.filtro_gradiente(image=image)
+            transformed_image = bP.escalamiento_abs(bP.filtro_gradiente(image=image))
+            transformed_image2 = bP.sumar_imagenes2(image, transformed_image)
             if(bP.are_images_equal(transformed_image, image)):
                 print("Son la misma imagen")
-            bP.display_results(image, transformed_image, "Filtro del gradiente")
+            bP.display_image(transformed_image, title="Filtro del gradiente puro")
+            bP.display_results(image, transformed_image2, "Filtro del gradiente")
 
         elif choice == '8' and 'image' in locals():
             bP.display_image(image)
