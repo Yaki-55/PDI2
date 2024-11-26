3de4944be20983b220100d4d6edf3bead49c5aa0
Yaki55 <mely020202@gs.utm.mx>
2024-11-26 08:55:29 -0600

    feat(transformadaFourier.py): calculates fft and fftshift for images

--- codigos/transformadaFourier.py
+++ codigos/transformadaFourier.py
@@ -7,7 +7,8 @@ img = cv.imread('../imagenesPractica4/cuadrado.png', cv.IMREAD_GRAYSCALE)
 assert img is not None, "No se pudo cargar la imagen"
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
+magnitude_spectrum = 20*np.log(np.abs(f))
+magnitude_spectrum2 = 20*np.log(np.abs(fshift))
  
-#bp.display_results(img, f, "Magnitud del espectro")
-bp.display_image(fshift, "Mangitud del espectro")
+bp.display_results(img ,magnitude_spectrum, "Mangitud del espectro")
+bp.display_results(img ,magnitude_spectrum2, "Mangitud del espectro")
