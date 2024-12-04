0b095063fd0a5de3da6c258f7151c3929991465b
Yaki55 <mely020202@gs.utm.mx>
2024-12-04 08:48:52 -0600

    Changes

---
+++ codigos/bibliotecaFrecuenciaPDI.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+def create_butterworth_low_pass_filter(width, height, d, n):
+    lp_filter = np.zeros((height, width, 2), np.float32)
+    centre = (width/2, height/2)
+    
+    for i in range(0, lp_filter.shape[1]):
+        for j in range(0, lp_filter.shape[0]):
+            radius = max(1, math.sqrt(math.pow((i-centre[0]), 2.0)+ math.pow))
+            lp_filter[j, i] = 1 / (1+math.pow((radius/d), (2*n)))
+    
+    return lp_filter
